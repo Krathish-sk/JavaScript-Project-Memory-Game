8d8d0feda0d095303e27f3b13d3b02c301cd415a
Krathish sk <krathishs.k123@gmail.com>
2023-01-11 19:40:41 +0530

    Added  image file & flipcard console functions

--- app.js
+++ app.js
@@ -1,28 +1,89 @@
 const cardArray=[{
-    name:'',
-    img:''
+    name:'blank',
+    img:'Images/blank.png'
 },
 {
-    name:'',
-    img:''
+    name:'cheeseburger',
+    img:'IMages/cheeseburger.png'
 },
 {
-    name:'',
-    img:''
+    name:'fries',
+    img:'Images/fries.png'
+},
+{
+    name:'hotdog',
+    img:'Images/hotdog.png'
+},
+{
+    name:'ice-creame',
+    img:'Images/ice-creame.png'
+},
+{
+    name:'milkshake',
+    img:'Images/milkshake.png'
+},
+{
+    name:'pizza',
+    img:'Images/pizza.png'
+},
+{
+    name:'white',
+    img:'Images/white.png'
+},
+{
+    name:'blank',
+    img:'Images/blank.png'
+},
+{
+    name:'cheeseburger',
+    img:'IMages/cheeseburger.png'
+},
+{
+    name:'fries',
+    img:'Images/fries.png'
+},
+{
+    name:'hotdog',
+    img:'Images/hotdog.png'
+},
+{
+    name:'ice-creame',
+    img:'Images/ice-creame.png'
+},
+{
+    name:'milkshake',
+    img:'Images/milkshake.png'
+},
+{
+    name:'pizza',
+    img:'Images/pizza.png'
+},
+{
+    name:'white',
+    img:'Images/white.png'
 }
 ]
 
-cardArrray.sort(() => 0.5 - Math.random());
+cardArray.sort(() => 0.5 - Math.random());
 
-const gridDisplay = document.querySelector('#grid');
+const grid = document.querySelector('#grid');
 
-function createBoard{
-    for(let i=1;i<=10;i++){
-        const card=document.createElement('img');
-        card.setAttribute('src','blank.png');
-        card.setAttribute('data-id',i);
-        gridDisplay.appendChild(card);
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement('img')
+      card.setAttribute('src', 'Images/blank.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
     }
-}
+  }
+createBoard();
 
-cretaeBorad();
+function flipCard(){
+    console.log(cardArray)
+    const cardID = this.getAttribute('data-id');
+    console.log(cardArray[cardID].name)
+    console.log(cardID);
+
+}
