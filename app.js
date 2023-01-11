a85656f4820a059f9bca776b03aabe682eb2ba61
Krathish sk <krathishs.k123@gmail.com>
2023-01-11 19:00:23 +0530

    Creating a a card element and adding data to it

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ const cardArray=[{
 
 cardArrray.sort(() => 0.5 - Math.random());
 
+const gridDisplay = document.querySelector('#grid');
+
+function createBoard{
+    for(let i=1;i<=10;i++){
+        const card=document.createElement('img');
+        card.setAttribute('src','blank.png');
+        card.setAttribute('data-id',i);
+        gridDisplay.appendChild(card);
+    }
+}
+
+cretaeBorad();
